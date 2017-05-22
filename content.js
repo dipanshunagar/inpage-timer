1430bcf5699a5e37b08c43b8c749c9debd45e754
Dipanshu Nagar <dipanshunagar@homail.com>
2017-05-22 21:42:27 +0530

    Draggable timer overlay, #1

--- content.js
+++ content.js
@@ -34,6 +34,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         //.appendChild(container);
         tick();
         timerObj = setInterval(tick, 1000);
+        addListeners();
         //alert('element added');
     }
     
@@ -69,3 +70,28 @@ function secondsToHms(d) {
     return m +':'+ s; 
 }
 
+var x_pos = 0,
+  y_pos = 0;
+
+function addListeners() {
+  document.getElementById('timer_container').addEventListener('mousedown', mouseDown, false);
+  window.addEventListener('mouseup', mouseUp, false);
+}
+
+function mouseUp() {
+  window.removeEventListener('mousemove', divMove, true);
+}
+
+function mouseDown(e) {
+  var div = document.getElementById('timer_container');
+  x_pos = e.clientX - div.offsetLeft;
+  y_pos = e.clientY - div.offsetTop;
+  window.addEventListener('mousemove', divMove, true);
+}
+
+function divMove(e) {
+  var div = document.getElementById('timer_container');
+  div.style.position = 'absolute';
+  div.style.top = (e.clientY - y_pos) + 'px';
+  div.style.left = (e.clientX - x_pos) + 'px';
+}
